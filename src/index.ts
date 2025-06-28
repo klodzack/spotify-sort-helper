3b26504804e377e10f345602ce1cd70be2f5a5ca
Zack Klodnicki <klod.zack@gmail.com>
2025-06-27 21:42:20 -0400

    fix: add albums

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,18 @@ import { SongList } from './SongList';
 
 (async () => {
     const list = new SongList([
-        /*
-        new Song('I Want to Hold Your Hand', 'The Beatles'),
-        new Song('A Hard Day\'s Night', 'The Beatles'),
-        new Song('Day Tripper', 'The Beatles'),
-        new Song('Ticket To Ride', 'The Beatles'),
-        new Song('Because', 'The Beatles'),
-        new Song('Back in the U.S.S.R.', 'The Beatles'),
-        new Song('Birthday', 'The Beatles'),
-        new Song('Blackbird', 'The Beatles'),
-        */
+        new Song('I Want to Hold Your Hand', 'Meet the Beatles!', 'The Beatles'),
+        new Song('A Hard Day\'s Night', 'A Hard Day\'s Night', 'The Beatles'),
+        new Song('Day Tripper', 'Rubber Soul', 'The Beatles'),
+        new Song('Ticket To Ride', 'Help!', 'The Beatles'),
+        new Song('Because', 'Abbey Road', 'The Beatles'),
+        new Song('Back in the U.S.S.R.', 'The Beatles (The White Album)', 'The Beatles'),
+        new Song('Birthday', 'The Beatles (The White Album)', 'The Beatles'),
+        new Song('Blackbird', 'The Beatles (The White Album)', 'The Beatles'),
+        new Song('Please Please Me', 'Please Please Me', 'The Beatles'),
         new Song('Boys', 'Please Please Me', 'The Beatles'),
-        /*
-        new Song('Daydream', 'The Lovin\' Spoonful'),
-        new Song('Daydream Believer', 'The Monkees'),
-        */
+        new Song('Daydream', 'Daydream', 'The Lovin\' Spoonful'),
+        new Song('Daydream Believer', 'The Birds, The Bees & The Monkees', 'The Monkees'),
     ]);
     const { genres, unknownSongs } = await list.getGenres();
     for (const genre of genres) {
