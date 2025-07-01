91656553418e928b23eb6410e30c7c1ad8387400
Zack Klodnicki <klod.zack@gmail.com>
2025-06-30 21:23:37 -0400

    feat: console output

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import { CsvInput } from './inputs/CsvInput';
 import { MemoryInput } from './inputs/MemoryInput';
+import { ConsoleOutput } from './outputs/ConsoleOutput';
 import { CsvOutput } from './outputs/CsvOutput';
 import { Processor } from './processing/Processor';
 import { Song } from './processing/Song';
 
 (async () => {
     const processor = new Processor({
-        // input: new CsvInput(new URL('examples/example.csv', 'file:' + __dirname)),
+        // input: new CsvInput(new URL('examples/large-example.csv', 'file:' + __dirname)),
         input: new MemoryInput([ new Song('Please Please Me', 'Please Please Me', 'The Beatles') ]),
-        output: new CsvOutput(new URL('out.csv', 'file:' + __dirname)),
+        // output: new CsvOutput(new URL('out.csv', 'file:' + __dirname)),
+        output: new ConsoleOutput(),
     });
     await processor.process();
 })().catch(e => {
---
+++ src/outputs/ConsoleOutput.ts
@@ -0,0 +1,29 @@
+import { consoleBottleneckLog } from "../bottlenecks/ConsoleBottleneck";
+import { IOutputRecord } from "../interfaces/IOutputRecord";
+import { ICategorizedOutput } from "./ICategorizedOutput";
+import chalk from 'chalk';
+
+export class ConsoleOutput implements ICategorizedOutput {
+    async write(stream: AsyncIterable<IOutputRecord>) {
+        for await (const outputRecord of stream) {
+            consoleBottleneckLog([
+                '',
+                `${chalk.blueBright('Title: ')} ${outputRecord.title}`,
+                ...(
+                    outputRecord.album
+                        ? [`${chalk.blueBright('Album: ')} ${outputRecord.album}`]
+                        : []
+                ),
+                `${chalk.blueBright('Artist: ')} ${outputRecord.artist}`,
+                ...(
+                    outputRecord.genres === null
+                        ? [`${chalk.blueBright('Genres: ')} ${chalk.redBright('UNKNOWN!')}`]
+                        : [
+                            chalk.blueBright('Genres:'),
+                            ...outputRecord.genres.map(g => `  - ${g}`)
+                        ]
+                )
+            ].join('\n'));
+        }
+    }
+}
